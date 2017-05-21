ceb2268b2c569977e7eb11b4ee9e6a9c83bff79a
pvpnvz <pvpnvz@users.noreply.github.com>
2017-05-21 16:00:14 +0800

    Change display-name of user from username to real_name

--- utils/templatetags/user.py
+++ utils/templatetags/user.py
@@ -9,9 +9,15 @@ def get_username(user_id):
     except User.DoesNotExist:
         return ""
 
+def get_real_name(user_id):
+    try:
+        return User.objects.get(id=user_id).real_name
+    except User.DoesNotExist:
+        return ""
+
 
 from django import template
 
 register = template.Library()
 register.filter("get_username", get_username)
-
+register.filter("get_real_name", get_real_name)
